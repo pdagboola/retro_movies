94cd04f6f86adf9d01caa487618141d7aaaf62ba
Peter Agboola <pdagboola@gmail.com>
2024-10-23 18:34:29 -0700

    2

--- db/pool.js
+++ db/pool.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: `postgresql://apple:password@localhost:5432/movies`,
+  connectionString: `postgresql://apple:password@localhost:5432/apple`,
 });
 
 module.exports = pool;
